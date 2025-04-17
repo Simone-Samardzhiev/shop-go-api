832a941e84d429e5a684d5727f73e7401737d14a
Simone <simonesamardzhiev@gmail.com>
2025-04-17 17:34:50 +0300

    Add api startup logic

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"api/config"
+	"github.com/gofiber/fiber/v2"
+	"log"
+)
 
+// API struct contains the application.
+type API struct {
+	// Conf stores the configuration of the app.
+	Conf *config.Config
+}
+
+// start mounts the handlers and binds the app to the specified port.
+func (a *API) start() error {
+	app := fiber.New()
+	return app.Listen(a.Conf.ApiConfig.ServerAddr)
+}
+
+// New create a new instance of API.
+func New() *API {
+	return &API{Conf: config.NewConfig()}
+}
+
+func main() {
+	api := New()
+	err := api.start()
+	log.Fatalf("Error starting the API with error: %v and configration: %+v", err, api.Conf)
 }
