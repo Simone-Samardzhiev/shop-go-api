72799015c076be9da0bdf2ec3317efb887fd1745
Simone <simonesamardzhiev@gmail.com>
2025-04-18 19:37:44 +0300

    Add TokenGroup model for returning both tokens on login

---
+++ models/token_group.go
@@ -0,0 +1,14 @@
+package models
+
+type TokenGroup struct {
+	RefreshToken string `json:"refresh_token"`
+	AccessToken  string `json:"access_token"`
+}
+
+// NewTokenGroup returns new instance of TokenGroup
+func NewTokenGroup(refreshToken, accessToken string) *TokenGroup {
+	return &TokenGroup{
+		RefreshToken: refreshToken,
+		AccessToken:  accessToken,
+	}
+}
