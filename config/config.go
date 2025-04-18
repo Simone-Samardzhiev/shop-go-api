cb5c3aad219364a206c9fa04198e659412e25dbf
Simone <simonesamardzhiev@gmail.com>
2025-04-18 10:15:55 +0300

    Add DatabaseConfig struct is config.go used to store database configuration. Add methods for easier parsing of environment variable for int and time.Duration

--- config/config.go
+++ config/config.go
@@ -1,6 +1,10 @@
 package config
 
-import "os"
+import (
+	"os"
+	"strconv"
+	"time"
+)
 
 // APIConfig holds API configuration
 type APIConfig struct {
@@ -10,9 +14,24 @@ type APIConfig struct {
 	IsDebug bool
 }
 
+// DatabaseConfig holds database configuration.
+type DatabaseConfig struct {
+	// URL used to connect to the database.
+	URL string
+	// MaxOpenConnections specifies the max open connection to the database.
+	MaxOpenConnections int
+	// MaxIdleConnections specifies the max open idle connections to the database.
+	MaxIdleConnections int
+	// MaxIdleTime specifies the time before idle connection is closed.
+	MaxIdleTime time.Duration
+	// MaxLifetime specifies the time a connection can be used.
+	MaxLifetime time.Duration
+}
+
 // Config holds all configuration into one place
 type Config struct {
 	ApiConfig *APIConfig
+	DbConfig  *DatabaseConfig
 }
 
 // NewConfig creates Config instance by
@@ -22,6 +41,13 @@ func NewConfig() *Config {
 		ApiConfig: &APIConfig{
 			ServerAddr: getEnvVar("SERVER_ADDR", ":8080"),
 			IsDebug:    getEnvVarBool("DEBUG", true),
+		},
+		DbConfig: &DatabaseConfig{
+			URL:                getEnvVar("DATABASE_URL", ""),
+			MaxOpenConnections: getEnvVarInt("MAX_OPEN_CONNECTIONS", 10),
+			MaxIdleConnections: getEnvVarInt("MAX_IDLE_CONNECTIONS", 10),
+			MaxLifetime:        getEnvVarDuration("MAX_LIFETIME", 5*time.Minute),
+			MaxIdleTime:        getEnvVarDuration("MAX_IDLE_TIME", 5*time.Minute),
 		},
 	}
 }
@@ -36,13 +62,44 @@ func getEnvVar(key, fallback string) string {
 	return fallback
 }
 
-// getEnvVarBool is specified version of getEnvVar special for bool variables.
-
+// getEnvVarBool is specialized version of getEnvVar for bool variables.
+//
 // If the variable exist the value is returned by checking if the string equals true
 // otherwise the fallback is returned.
 func getEnvVarBool(key string, fallback bool) bool {
 	if value, ok := os.LookupEnv(key); ok {
 		return value == "true"
 	}
+	return fallback
+}
+
+// getEnvVarInt is specialized version of getEnvVar for int variables.
+//
+// If the variable exist, and it is a valid int the value is returned otherwise
+// the fallback is returned
+func getEnvVarInt(key string, fallback int) int {
+	if value, ok := os.LookupEnv(key); ok {
+		result, err := strconv.Atoi(value)
+		if err != nil {
+			return fallback
+		}
+		return result
+	}
+	return fallback
+}
+
+// getEnvVarDuration is specialized version of getEnvVar for time.Duration variables.
+//
+// If the variable exists, and it is a valid int the value is parsed into duration and returned
+// otherwise the fallback is returned.
+func getEnvVarDuration(key string, fallback time.Duration) time.Duration {
+	if value, ok := os.LookupEnv(key); ok {
+		result, err := strconv.Atoi(value)
+		if err != nil {
+			return fallback
+		}
+		return time.Duration(result)
+	}
+
 	return fallback
 }
