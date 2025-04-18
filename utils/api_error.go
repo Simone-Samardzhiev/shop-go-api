87b3544e3d0443161a71c7478487b675a2cabae4
Simone <simonesamardzhiev@gmail.com>
2025-04-18 14:11:24 +0300

    Add new APIError for internal server error

--- utils/api_error.go
+++ utils/api_error.go
@@ -1,5 +1,9 @@
 package utils
 
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
 // APIError is the standard way of returning an error.
 type APIError struct {
 	Message string
@@ -9,4 +13,9 @@ type APIError struct {
 // NewAPIError return instance of APIError
 func NewAPIError(msg string, status int) *APIError {
 	return &APIError{msg, status}
+}
+
+// InternalServerAPIError is the standard way of return a server error
+func InternalServerAPIError() *APIError {
+	return &APIError{"Internal Server Error", fiber.StatusInternalServerError}
 }
