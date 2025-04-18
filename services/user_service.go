884ed450f169971e1a3bfe84b5c287996415e5ac
Simone <simonesamardzhiev@gmail.com>
2025-04-18 14:29:00 +0300

    Add UserService that manages user business logic

--- services/user_service.go
+++ services/user_service.go
@@ -6,6 +6,7 @@ import (
 	"api/utils"
 	"context"
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 )
 
 type UserService interface {
@@ -21,4 +22,16 @@ func (s *DefaultUserService) AddUser(ctx context.Context, payload models.Registe
 		return utils.NewAPIError("Invalid User Payload", fiber.StatusBadRequest)
 	}
 
+	result, err := s.repo.CheckEmailAndUsername(ctx, payload.Email, payload.Username)
+	if result {
+		return utils.NewAPIError("User already exists", fiber.StatusConflict)
+	}
+
+	user := models.NewUser(uuid.New(), payload.Email, payload.Username, payload.Password, models.Client)
+	err = s.repo.AddUser(ctx, user)
+	if err != nil {
+		return utils.InternalServerAPIError()
+	}
+
+	return nil
 }
