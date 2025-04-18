534eab3a937643b305522d2a142228a4434e5bb1
Simone <simonesamardzhiev@gmail.com>
2025-04-18 19:32:43 +0300

    Add jwt.go used for creating token and move the claims there

--- auth/jwt.go
+++ auth/jwt.go
@@ -1,4 +1,56 @@
 package auth
 
+import (
+	"api/config"
+	"api/models"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"time"
+)
+
+// TokenType used to set the type of claims
+type TokenType = int
+
+const (
+	// RefreshToken used to revalidate tokens.
+	RefreshToken TokenType = iota
+	// AccessToken used to get access to API.
+	AccessToken
+)
+
+// Claims is a custom implementation of jwt.Claims
+type Claims struct {
+	TokenType TokenType       `json:"token_type"`
+	Role      models.UserType `json:"role"`
+	jwt.RegisteredClaims
+}
+
+// JWTAuthenticator used to create JWT.
 type JWTAuthenticator struct {
+	conf config.AuthConfig
+}
+
+// CreateAccessToken creates a new access token and signs it.
+func (a *JWTAuthenticator) CreateAccessToken(sub uuid.UUID, role models.UserType, tokenType TokenType, exp time.Time) (string, error) {
+	claims := Claims{
+		TokenType: tokenType,
+		Role:      role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    a.conf.Issuer,
+			Subject:   sub.String(),
+			Audience:  []string{role},
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			ExpiresAt: jwt.NewNumericDate(exp),
+			NotBefore: jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, err := token.SignedString(a.conf.JWTSecret)
+	return signedToken, err
+}
+
+// NewJWTAuthenticator creates a new instance of JWTAuthenticator
+func NewJWTAuthenticator(conf config.AuthConfig) *JWTAuthenticator {
+	return &JWTAuthenticator{conf: conf}
 }
