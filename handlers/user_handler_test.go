34a5fb6848f825d786a4cb6c527555ef0397aec1
Simone <simonesamardzhiev@gmail.com>
2025-04-19 10:02:58 +0300

    Improve testing in user_handler_test.go

--- handlers/user_handler_test.go
+++ handlers/user_handler_test.go
@@ -9,11 +9,13 @@ import (
 	"bytes"
 	"encoding/json"
 	"github.com/gofiber/fiber/v2"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
+// setup used for easier creation of DefaultUserHandler
 func setup() *DefaultUserHandler {
 	userRepository := repositories.NewMemoryUserRepository()
 	authenticator := auth.NewJWTAuthenticator(config.AuthConfig{
@@ -25,21 +27,38 @@ func setup() *DefaultUserHandler {
 	return NewDefaultUserHandler(service)
 }
 
+// validUser returns io.Reader to a valid user.
+func validUser() (io.Reader, error) {
+	user := models.NewRegisterClientPayload("validEmail@gmail.com", "ValidUsername", "ValidPassword_1")
+	data, err := json.Marshal(user)
+	return bytes.NewReader(data), err
+}
+
+// validUser returns io.Reader to a invalid user.
+func invalidUser() (io.Reader, error) {
+	user := models.NewRegisterClientPayload("invalid", "ValidUsername", "ValidPassword_1")
+	data, err := json.Marshal(user)
+	return bytes.NewReader(data), err
+}
+
+// TestDefaultUserHandlerRegisterClient test if registration with valid payload works.
 func TestDefaultUserHandlerRegisterClient(t *testing.T) {
+	// Creating the app.
 	handler := setup()
 	app := fiber.New()
 	app.Post("/", handler.RegisterClient())
 
-	user := models.NewRegisterClientPayload("validEmail@gmail.com", "ValidUsername", "ValidPassword_1")
-	data, err := json.Marshal(user)
+	user, err := validUser()
 	if err != nil {
-		t.Errorf("Failed to marshal user data")
+		t.Errorf("Error creating a valid user: %v", err)
 	}
 
-	req := httptest.NewRequest(http.MethodPost, "/", bytes.NewReader(data))
+	// Creating a new request.
+	req := httptest.NewRequest(http.MethodPost, "/", user)
 	req.Header.Set("Content-Type", "application/json")
 	resp, err := app.Test(req, -1)
 
+	// Checking if the request is successful.
 	if err != nil {
 		t.Errorf("Failed to make request: %v", err)
 	}
@@ -49,20 +68,22 @@ func TestDefaultUserHandlerRegisterClient(t *testing.T) {
 }
 
 func TestDefaultUserHandlerRegisterClientWithInvalidUser(t *testing.T) {
+	// Creating an app.
 	handler := setup()
 	app := fiber.New()
 	app.Post("/", handler.RegisterClient())
 
-	user := models.NewRegisterClientPayload("invalid@gmail", "ValidUsername", "ValidPassword_1")
-	data, err := json.Marshal(user)
+	user, err := invalidUser()
 	if err != nil {
-		t.Errorf("Failed to marshal user data")
+		t.Errorf("Error creating invalid: %v", err)
 	}
 
-	req := httptest.NewRequest(http.MethodPost, "/", bytes.NewReader(data))
+	// Creating a new request.
+	req := httptest.NewRequest(http.MethodPost, "/", user)
 	req.Header.Set("Content-Type", "application/json")
 	resp, err := app.Test(req, -1)
 
+	// Check if the status code is expected.
 	if err != nil {
 		t.Errorf("Failed to make request: %v", err)
 	}
@@ -71,21 +92,64 @@ func TestDefaultUserHandlerRegisterClientWithInvalidUser(t *testing.T) {
 	}
 }
 
+// TestDefaultUserHandlerRegisterWithSameEmailOrCredentials if the handler returns an error
+// if the user tries registering with the same email or username.
+func TestDefaultUserHandlerRegisterWithSameEmailOrCredentials(t *testing.T) {
+	handler := setup()
+	app := fiber.New()
+	app.Post("/", handler.RegisterClient())
+
+	user, err := validUser()
+	if err != nil {
+		t.Errorf("Error creating a valid user: %v", err)
+	}
+
+	// Making a new register request.
+	req := httptest.NewRequest(http.MethodPost, "/", user)
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := app.Test(req, -1)
+
+	// Check if the request was successful.
+	if err != nil {
+		t.Errorf("Failed to make request: %v", err)
+	}
+	if resp.StatusCode != fiber.StatusCreated {
+		t.Errorf("Unexpected response code: %v", resp.StatusCode)
+	}
+
+	// Send the same request with the same credentials.
+	req = httptest.NewRequest(http.MethodPost, "/", user)
+	req.Header.Set("Content-Type", "application/json")
+	resp, err = app.Test(req, -1)
+
+	// Check if the request was failed with StatusConflict.
+	if err != nil {
+		t.Errorf("Failed to make request: %v", err)
+	}
+	if resp.StatusCode != fiber.StatusConflict {
+		t.Errorf("Unexpected response code: %v", resp.StatusCode)
+	}
+}
+
+// TestDefaultUserHandlerLogin tests if login handler works expectedly.
 func TestDefaultUserHandlerLogin(t *testing.T) {
+	// Creating the app.
 	handler := setup()
 	app := fiber.New()
 	app.Post("/register", handler.RegisterClient())
 	app.Post("/login", handler.Login())
 
-	user := models.NewRegisterClientPayload("validEmail@gmail.com", "ValidUsername", "ValidPassword_1")
-	data, err := json.Marshal(user)
+	user, err := validUser()
 	if err != nil {
-		t.Errorf("Failed to marshal user data")
+		t.Errorf("Error creating a valid user: %v", err)
 	}
 
-	req := httptest.NewRequest(http.MethodPost, "/register", bytes.NewReader(data))
+	// Creating a new request to register.
+	req := httptest.NewRequest(http.MethodPost, "/register", user)
 	req.Header.Set("Content-Type", "application/json")
 	resp, err := app.Test(req, -1)
+
+	//  Check if the request is successful.
 	if err != nil {
 		t.Errorf("Failed to make request for registering: %v", err)
 	}
@@ -93,9 +157,12 @@ func TestDefaultUserHandlerLogin(t *testing.T) {
 		t.Errorf("Unexpected response code: %v", resp.StatusCode)
 	}
 
-	req = httptest.NewRequest(http.MethodPost, "/login", bytes.NewReader(data))
+	// Creating a new request for login.
+	req = httptest.NewRequest(http.MethodPost, "/login", user)
 	req.Header.Set("Content-Type", "application/json")
 	resp, err = app.Test(req, -1)
+
+	//  Check if the request is successful.
 	if err != nil {
 		t.Errorf("Failed to make request for login: %v", err)
 	}
