18edda25cab5cfce738484bf6b2d721eca4bb14a
Simone <simonesamardzhiev@gmail.com>
2025-04-19 09:45:49 +0300

    Add testing for login method in UserHandler

--- handlers/user_handler_test.go
+++ handlers/user_handler_test.go
@@ -70,3 +70,36 @@ func TestDefaultUserHandlerRegisterClientWithInvalidUser(t *testing.T) {
 		t.Errorf("Unexpected response code: %v", resp.StatusCode)
 	}
 }
+
+func TestDefaultUserHandlerLogin(t *testing.T) {
+	handler := setup()
+	app := fiber.New()
+	app.Post("/register", handler.RegisterClient())
+	app.Post("/login", handler.Login())
+
+	user := models.NewRegisterClientPayload("validEmail@gmail.com", "ValidUsername", "ValidPassword_1")
+	data, err := json.Marshal(user)
+	if err != nil {
+		t.Errorf("Failed to marshal user data")
+	}
+
+	req := httptest.NewRequest(http.MethodPost, "/register", bytes.NewReader(data))
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := app.Test(req, -1)
+	if err != nil {
+		t.Errorf("Failed to make request for registering: %v", err)
+	}
+	if resp.StatusCode != fiber.StatusCreated {
+		t.Errorf("Unexpected response code: %v", resp.StatusCode)
+	}
+
+	req = httptest.NewRequest(http.MethodPost, "/login", bytes.NewReader(data))
+	req.Header.Set("Content-Type", "application/json")
+	resp, err = app.Test(req, -1)
+	if err != nil {
+		t.Errorf("Failed to make request for login: %v", err)
+	}
+	if resp.StatusCode != fiber.StatusOK {
+		t.Errorf("Unexpected response code: %v", resp.StatusCode)
+	}
+}
