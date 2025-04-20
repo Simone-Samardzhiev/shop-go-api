d568fa34fa376a645c8c395df9e970c977e78f33
Simone <simonesamardzhiev@gmail.com>
2025-04-20 15:11:35 +0300

    Improve user_service_test.go

--- services/user_service_test.go
+++ services/user_service_test.go
@@ -9,7 +9,11 @@ import (
 	"testing"
 )
 
-func setup() UserService {
+// Setup returns DefaultUserService
+//
+// The service is configured with repositories.MemoryUserRepository,
+// repositories.MemoryTokenRepository and auth.JWTAuthenticator.
+func Setup() *DefaultUserService {
 	userRepository := repositories.NewMemoryUserRepository()
 	tokenRepository := repositories.NewMemoryTokenRepository()
 	authenticator := auth.NewJWTAuthenticator(config.AuthConfig{
@@ -19,51 +23,55 @@ func setup() UserService {
 	return NewDefaultUserService(userRepository, tokenRepository, authenticator)
 }
 
+// TestDefaultUserServiceAddValidUser verifies that adding a valid user succeeds, and
+// an attempt to add the same user result in an error.
 func TestDefaultUserServiceAddValidUser(t *testing.T) {
-	service := setup()
+	service := Setup()
+
 	user := models.NewRegisterClientPayload("validEmail@gmail.com", "ValidUsername", "ValidPassword_2")
 
+	// The First attempt should succeed.
 	err := service.AddClient(context.Background(), user)
 	if err != nil {
 		t.Errorf("AddClient returned error: %v", err)
 	}
 
+	// The second attempt should fail.
 	err = service.AddClient(context.Background(), user)
 	if err == nil {
 		t.Errorf("AddClient returned no error when adding the same user.")
 	}
 }
 
+// TestDefaultUserServiceAddInvalidUser verifies that adding an invalid user result in an error.
 func TestDefaultUserServiceAddInvalidUser(t *testing.T) {
-	service := setup()
+	service := Setup()
 	user := models.NewRegisterClientPayload("valid.com", "ValidUsername", "ValidPassword_2")
 
+	// Adding the invalid user should result in an error.
 	err := service.AddClient(context.Background(), user)
 	if err == nil {
 		t.Errorf("AddClient returned no error when adding invalid user.")
 	}
 }
 
+// TestDefaultUserServiceAddLogin verifies that after successful
+// registration, the user can log in.
 func TestDefaultUserServiceAddLogin(t *testing.T) {
-	service := setup()
+	service := Setup()
 
+	// Registering the user.
 	user := models.NewRegisterClientPayload("validEmail@gmail.com", "ValidUsername", "ValidPassword_2")
-
 	err := service.AddClient(context.Background(), user)
 	if err != nil {
 		t.Errorf("AddClient returned error: %v", err)
 	}
 
-	err = service.AddClient(context.Background(), user)
-	if err == nil {
-		t.Errorf("AddClient returned no error when adding the same user.")
-	}
-
+	// Logging as the same user.
 	loginUser := models.LoginUserPayload{
 		Username: "ValidUsername",
 		Password: "ValidPassword_2",
 	}
-
 	_, err = service.Login(context.Background(), &loginUser)
 	if err != nil {
 		t.Errorf("Error logging in: %v", err)
