6a4c740637424800a3c995f1d6704bb826ed9fe1
Simone <simonesamardzhiev@gmail.com>
2025-06-21 16:22:40 +0300

    Test: Add the `FilterPathValue` function.

--- testutils/filters.go
+++ testutils/filters.go
@@ -17,3 +17,20 @@ func FilterToken(token string) string {
 	}
 	return result
 }
+
+// FilterPathValue filter path values so they can be inserted safely in urls.
+func FilterPathValue(path string) string {
+	builder := strings.Builder{}
+
+	for _, c := range path {
+		if (c >= 48 && c <= 57) || (c >= 65 && c <= 90) || (c >= 97 && c <= 122) || c == 45 || c == 46 || c == 95 || c == 126 {
+			builder.WriteRune(c)
+		}
+	}
+
+	result := builder.String()
+	if len(result) > 1024 {
+		return result[:1024]
+	}
+	return result
+}
