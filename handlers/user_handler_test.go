e077f0d6eb62ee912b2187094a508d544709c511
Simone <simonesamardzhiev@gmail.com>
2025-06-21 14:43:52 +0300

    Test: Add testing for `DefaultUserHandler`.

---
+++ handlers/user_handler_test.go
@@ -0,0 +1,620 @@
+package handlers_test
+
+import (
+	"api/auth"
+	"api/config"
+	"api/handlers"
+	"api/models"
+	"api/repositories"
+	"api/services"
+	"api/testutils"
+	"api/utils"
+	"encoding/json"
+	jwtware "github.com/gofiber/contrib/jwt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"net/http"
+	"net/url"
+	"strconv"
+	"testing"
+)
+
+// DefaultUserHandler creates a new instance of DefaultUserHandler with users.
+func DefaultUserHandler() (*handlers.DefaultUserHandler, error) {
+	userRepo, err := testutils.NewMemoryUserRepositoryWithUsers()
+	if err != nil {
+		return nil, err
+	}
+	tokenRepo := repositories.NewMemoryTokenRepository(nil)
+	authenticator := auth.NewJWTAuthenticator(config.AuthConfig{
+		JWTSecret: "secret",
+		Issuer:    "test",
+	})
+	service := services.NewDefaultUserService(userRepo, tokenRepo, authenticator)
+	return handlers.NewDefaultUserHandler(service), nil
+}
+
+// Middleware returns JWT middleware.
+func Middleware() fiber.Handler {
+	return jwtware.New(jwtware.Config{
+		Claims: &auth.Claims{},
+		SigningKey: jwtware.SigningKey{
+			JWTAlg: jwt.SigningMethodHS256.Alg(),
+			Key:    []byte("secret"),
+		},
+		SuccessHandler: func(c *fiber.Ctx) error {
+			claims := c.Locals("user").(*jwt.Token).Claims.(*auth.Claims)
+			c.Locals("user", claims)
+			return c.Next()
+		},
+		ErrorHandler: func(c *fiber.Ctx, err error) error {
+			return c.Status(http.StatusUnauthorized).JSON(utils.InvalidTokenAPIError())
+		},
+	})
+}
+
+func TestDefaultUserHandler_RegisterClient(t *testing.T) {
+	handler, err := DefaultUserHandler()
+	if err != nil {
+		t.Fatalf("Error creating user handler: %v", err)
+	}
+	app := fiber.New()
+	app.Post("/register", handler.RegisterClient())
+
+	tests := []struct {
+		name           string
+		body           *models.RegisterClientPayload
+		expectedStatus int
+	}{
+		{
+			name:           "Send valid payload",
+			body:           models.NewRegisterClientPayload("NewUser@example.com", "NewUser123", "Strong_12"),
+			expectedStatus: fiber.StatusCreated,
+		}, {
+			name:           "Send invalid payload",
+			body:           models.NewRegisterClientPayload("", "", ""),
+			expectedStatus: fiber.StatusBadRequest,
+		}, {
+			name:           "Send valid payload with duplicate email",
+			body:           models.NewRegisterClientPayload("NewUser@example.com", "NewUser321", "Strong_12"),
+			expectedStatus: fiber.StatusConflict,
+		}, {
+			name:           "Send valid payload with duplicate username",
+			body:           models.NewRegisterClientPayload("NewUser@email.com", "NewUser123", "Strong_12"),
+			expectedStatus: fiber.StatusConflict,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			res, requestErr := testutils.SendRequest(app, "/register", "POST", "", test.body)
+			if requestErr != nil {
+				t.Errorf("Error sending request: %v", requestErr)
+			}
+
+			if res.StatusCode != test.expectedStatus {
+				t.Errorf("Expected status %d, got %d", test.expectedStatus, res.StatusCode)
+			}
+		})
+	}
+
+}
+
+func BenchmarkDefaultUserHandler_RegisterClient(b *testing.B) {
+	handler, err := DefaultUserHandler()
+	if err != nil {
+		b.Fatalf("Error creating user handler: %v", err)
+	}
+
+	app := fiber.New()
+	app.Post("/register", handler.RegisterClient())
+
+	for b.Loop() {
+		id := uuid.New().String()
+		email := id + "@example.com"
+		username := id + "username"
+		password := id + "<PASSWORD>"
+		body := models.NewRegisterClientPayload(email, username, password)
+
+		_, requestErr := testutils.SendRequest(app, "/register", "POST", "", body)
+		if requestErr != nil {
+			b.Errorf("Error sending request: %v", err)
+		}
+	}
+}
+
+func FuzzDefaultUserHandler_RegisterClient(f *testing.F) {
+	handler, err := DefaultUserHandler()
+	if err != nil {
+		f.Fatalf("Error creating user handler: %v", err)
+	}
+	app := fiber.New()
+	app.Post("/register", handler.RegisterClient())
+
+	f.Add("email", "username", "password")
+	f.Add("", "", "")
+	f.Add("validEmail@example.com", "ValidUsername", "String_pass123")
+
+	f.Fuzz(func(t *testing.T, email, username, password string) {
+		body := models.NewRegisterClientPayload(email, username, password)
+		_, requestErr := testutils.SendRequest(app, "/register", "POST", "", body)
+		if requestErr != nil {
+			t.Errorf("Error sending request: %v", err)
+		}
+	})
+}
+
+func TestDefaultUserHandler_RegisterUser(t *testing.T) {
+	handler, err := DefaultUserHandler()
+	if err != nil {
+		t.Fatalf("Error creating user handler: %v", err)
+	}
+	app := fiber.New()
+	app.Post("/register", Middleware(), handler.RegisterUser())
+	app.Post("/login", handler.Login())
+	tokens, err := testutils.LoginAsAdmin(app, "/login")
+	if err != nil {
+		t.Fatalf("Error logging in as admin: %v", err)
+	}
+
+	tests := []struct {
+		name           string
+		payload        *models.RegisterUserPayload
+		token          string
+		expectedStatus int
+	}{
+		{
+			name:           "Register valid user",
+			payload:        models.NewRegisterUserPayload("example1@email.com", "ValidUsername1", "Strong_pass123", models.Client),
+			token:          tokens.AccessToken,
+			expectedStatus: fiber.StatusCreated,
+		}, {
+			name:           "Register valid user with duplicate email",
+			payload:        models.NewRegisterUserPayload("example1@email.com", "ValidUsername2", "Strong_pass123", models.Client),
+			token:          tokens.AccessToken,
+			expectedStatus: fiber.StatusConflict,
+		}, {
+			name:           "Register valid user with duplicate username",
+			payload:        models.NewRegisterUserPayload("example2@email.com", "ValidUsername1", "Strong_pass123", models.Client),
+			token:          tokens.AccessToken,
+			expectedStatus: fiber.StatusConflict,
+		}, {
+			name:           "Register user with invalid payload",
+			payload:        models.NewRegisterUserPayload("", "", "", ""),
+			token:          tokens.AccessToken,
+			expectedStatus: fiber.StatusBadRequest,
+		}, {
+			name:           "Register user with invalid token",
+			payload:        models.NewRegisterUserPayload("example2@email.com", "ValidUsername2", "Strong_pass123", models.Client),
+			token:          "",
+			expectedStatus: fiber.StatusUnauthorized,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			res, requestErr := testutils.SendRequest(app, "/register", "POST", test.token, test.payload)
+
+			if requestErr != nil {
+				t.Errorf("Error sending request: %v", requestErr)
+			}
+			if res.StatusCode != test.expectedStatus {
+				t.Errorf("Expected status %d, got %d", test.expectedStatus, res.StatusCode)
+			}
+		})
+	}
+}
+
+func BenchmarkDefaultUserHandler_RegisterUser(b *testing.B) {
+	handler, err := DefaultUserHandler()
+	if err != nil {
+		b.Fatalf("Error creating user handler: %v", err)
+	}
+	app := fiber.New()
+	app.Post("/register", Middleware(), handler.RegisterUser())
+	app.Post("/login", handler.Login())
+	tokens, err := testutils.LoginAsAdmin(app, "/login")
+	if err != nil {
+		b.Fatalf("Error logging in as admin: %v", err)
+	}
+
+	for b.Loop() {
+		id := uuid.New().String()
+		email := id + "@example.com"
+		username := id + "username"
+		password := id + "String_pass123"
+		body := models.NewRegisterUserPayload(email, username, password, models.Client)
+		_, requestErr := testutils.SendRequest(app, "/register", "POST", tokens.AccessToken, body)
+		if requestErr != nil {
+			b.Errorf("Error sending request: %v", err)
+		}
+	}
+}
+
+func FuzzDefaultUserHandler_RegisterUser(f *testing.F) {
+	handler, err := DefaultUserHandler()
+	if err != nil {
+		f.Fatalf("Error creating user handler: %v", err)
+	}
+	app := fiber.New()
+	app.Post("/register", Middleware(), handler.RegisterUser())
+	app.Post("/login", handler.Login())
+	tokens, err := testutils.LoginAsAdmin(app, "/login")
+	if err != nil {
+		f.Fatalf("Error logging in as admin: %v", err)
+	}
+
+	f.Add("email", "username", "password", "role", tokens.AccessToken)
+	f.Add("", "", "", "", tokens.AccessToken)
+	f.Add("sda", "dasda", "qweq", "", "")
+
+	f.Fuzz(func(t *testing.T, email, username, password, role, token string) {
+		token = testutils.FilterToken(token)
+
+		body := models.NewRegisterUserPayload(email, username, password, models.Client)
+		_, requestErr := testutils.SendRequest(app, "/register", "POST", token, body)
+		if requestErr != nil {
+			t.Errorf("Error sending request: %v", err)
+		}
+	})
+}
+
+func TestDefaultUserHandler_Login(t *testing.T) {
+	handler, err := DefaultUserHandler()
+	if err != nil {
+		t.Fatalf("Error creating user handler: %v", err)
+	}
+	app := fiber.New()
+	app.Post("/login", handler.Login())
+
+	tests := []struct {
+		name           string
+		payload        *models.LoginUserPayload
+		expectedStatus int
+	}{
+		{
+			name:           "Login with valid admin credentials",
+			payload:        models.NewLoginUserPayload("john_doe", "Password1!"),
+			expectedStatus: fiber.StatusOK,
+		}, {
+			name:           "Login with invalid client credentials",
+			payload:        models.NewLoginUserPayload("jane_s", "SecurePass2@"),
+			expectedStatus: fiber.StatusOK,
+		}, {
+			name:           "Login with valid delivery credentials",
+			payload:        models.NewLoginUserPayload("alexw", "MySecretPass3#"),
+			expectedStatus: fiber.StatusOK,
+		}, {
+			name:           "Login with valid workshop credentials",
+			payload:        models.NewLoginUserPayload("emmad", "Password4$"),
+			expectedStatus: fiber.StatusOK,
+		}, {
+			name:           "Login with invalid credentials",
+			payload:        models.NewLoginUserPayload("", ""),
+			expectedStatus: fiber.StatusUnauthorized,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			res, requestErr := testutils.SendRequest(app, "/login", "POST", "", test.payload)
+			if requestErr != nil {
+				t.Errorf("Error sending request: %v", requestErr)
+			}
+			if res.StatusCode != test.expectedStatus {
+				t.Errorf("Expected status %d, got %d", test.expectedStatus, res.StatusCode)
+			}
+
+			var tokens models.TokenGroup
+			decoderErr := json.NewDecoder(res.Body).Decode(&tokens)
+			if decoderErr != nil {
+				t.Errorf("Error decoding response: %v", decoderErr)
+			}
+		})
+	}
+}
+
+func BenchmarkDefaultUserHandler_Login(b *testing.B) {
+	handler, err := DefaultUserHandler()
+	if err != nil {
+		b.Fatalf("Error creating user handler: %v", err)
+	}
+	app := fiber.New()
+	app.Post("/login", handler.Login())
+
+	for b.Loop() {
+		_, requestErr := testutils.SendRequest(app, "/login", "POST", "", models.NewLoginUserPayload("john_doe", "Password1!"))
+		if requestErr != nil {
+			b.Errorf("Error sending request: %v", requestErr)
+		}
+	}
+}
+
+func FuzzDefaultUserHandler_Login(f *testing.F) {
+	handler, err := DefaultUserHandler()
+	if err != nil {
+		f.Fatalf("Error creating user handler: %v", err)
+	}
+	app := fiber.New()
+	app.Post("/login", handler.Login())
+
+	f.Add("username", "password")
+	f.Add("", "")
+	f.Fuzz(func(t *testing.T, username, password string) {
+		_, requestErr := testutils.SendRequest(app, "/login", "POST", "", models.NewLoginUserPayload(username, password))
+		if requestErr != nil {
+			t.Errorf("Error sending request: %v", requestErr)
+		}
+	})
+}
+
+func TestDefaultUserHandler_RefreshSession(t *testing.T) {
+	handler, err := DefaultUserHandler()
+	if err != nil {
+		t.Fatalf("Error creating user handler: %v", err)
+	}
+	app := fiber.New()
+	app.Post("/login", handler.Login())
+	app.Post("/refresh", Middleware(), handler.RefreshSession())
+	tokens, err := testutils.LoginAsAdmin(app, "/login")
+	if err != nil {
+		t.Fatalf("Error logging in as admin: %v", err)
+	}
+
+	tests := []struct {
+		name           string
+		token          string
+		expectedStatus int
+	}{
+		{
+			name:           "Refresh with valid token",
+			token:          tokens.RefreshToken,
+			expectedStatus: fiber.StatusOK,
+		}, {
+			name:           "Refresh with invalid token",
+			token:          "",
+			expectedStatus: fiber.StatusUnauthorized,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			res, requestErr := testutils.SendRequest(app, "/refresh", "POST", test.token, nil)
+			if requestErr != nil {
+				t.Errorf("Error sending request: %v", requestErr)
+			}
+			if res.StatusCode != test.expectedStatus {
+				t.Errorf("Expected status %d, got %d", test.expectedStatus, res.StatusCode)
+			}
+
+			var receivedTokens models.TokenGroup
+			decoderErr := json.NewDecoder(res.Body).Decode(&receivedTokens)
+			if decoderErr != nil {
+				t.Errorf("Error decoding response: %v", decoderErr)
+			}
+		})
+	}
+}
+
+func BenchmarkDefaultUserHandler_RefreshSession(b *testing.B) {
+	handler, err := DefaultUserHandler()
+	if err != nil {
+		b.Fatalf("Error creating user handler: %v", err)
+	}
+	app := fiber.New()
+	app.Post("/login", handler.Login())
+	app.Post("/refresh", handler.RefreshSession())
+	tokens, err := testutils.LoginAsAdmin(app, "/login")
+	if err != nil {
+		b.Fatalf("Error logging in as admin: %v", err)
+	}
+
+	for b.Loop() {
+		res, requestErr := testutils.SendRequest(app, "/refresh", "POST", tokens.RefreshToken, nil)
+		if requestErr != nil {
+			b.Errorf("Error sending request: %v", requestErr)
+		}
+
+		var receivedTokens models.TokenGroup
+		decoderErr := json.NewDecoder(res.Body).Decode(&receivedTokens)
+		if decoderErr != nil {
+			b.Errorf("Error decoding response: %v", decoderErr)
+		}
+		tokens.RefreshToken = receivedTokens.RefreshToken
+	}
+}
+
+func FuzzDefaultUserHandler_RefreshSession(f *testing.F) {
+	handler, err := DefaultUserHandler()
+	if err != nil {
+		f.Fatalf("Error creating user handler: %v", err)
+	}
+	app := fiber.New()
+	app.Post("/login", handler.Login())
+	app.Post("/refresh", Middleware(), handler.RefreshSession())
+	tokens, err := testutils.LoginAsAdmin(app, "/login")
+	if err != nil {
+		f.Fatalf("Error logging in as admin: %v", err)
+	}
+
+	f.Add(tokens.RefreshToken)
+	f.Add(tokens.AccessToken)
+	f.Add("")
+	f.Add("token")
+
+	f.Fuzz(func(t *testing.T, token string) {
+		token = testutils.FilterToken(token)
+		_, requestErr := testutils.SendRequest(app, "/refresh", "POST", token, nil)
+		if requestErr != nil {
+			t.Errorf("Error sending request: %v", requestErr)
+		}
+	})
+}
+
+func TestDefaultUserHandler_GetUsers(t *testing.T) {
+	handler, err := DefaultUserHandler()
+	if err != nil {
+		t.Fatalf("Error creating user handler: %v", err)
+	}
+	app := fiber.New()
+	app.Post("/login", handler.Login())
+	app.Get("/users", Middleware(), handler.GetUsers())
+	tokens, err := testutils.LoginAsAdmin(app, "/login")
+	if err != nil {
+		t.Fatalf("Error logging in as admin: %v", err)
+	}
+
+	role := models.Client
+
+	tests := []struct {
+		name           string
+		token          string
+		limit          int
+		page           int
+		role           *string
+		expectedStatus int
+		expectedEmails []string
+	}{
+		{
+			name:           "Get users(limit=4, page=1, role=nil)",
+			token:          tokens.AccessToken,
+			limit:          4,
+			page:           1,
+			role:           nil,
+			expectedStatus: fiber.StatusOK,
+			expectedEmails: []string{"user1@example.com", "jane_smith@example.com", "alex.wilson@example.com", "emma.davis@example.com"},
+		}, {
+			name:           "Get users(limit=4, page=2, role=nil)",
+			token:          tokens.AccessToken,
+			limit:          4,
+			page:           2,
+			role:           nil,
+			expectedStatus: fiber.StatusOK,
+			expectedEmails: []string{"michael.brown@example.com", "olivia.jones@example.com", "william.garcia@example.com", "sophia.rodriguez@example.com"},
+		}, {
+			name:           "Get users(limit=4, page=1, role=client)",
+			token:          tokens.AccessToken,
+			limit:          4,
+			page:           1,
+			role:           &role,
+			expectedStatus: fiber.StatusOK,
+			expectedEmails: []string{"jane_smith@example.com", "olivia.jones@example.com", "isabella.hernandez@example.com", "chloe.sanchez@example.com"},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			params := url.Values{}
+			params.Add("limit", strconv.Itoa(test.limit))
+			params.Add("page", strconv.Itoa(test.page))
+			if test.role != nil {
+				params.Add("role", *test.role)
+			}
+
+			fullUrl := "/users?" + params.Encode()
+			res, requestErr := testutils.SendRequest(app, fullUrl, "GET", test.token, nil)
+			if requestErr != nil {
+				t.Errorf("Error sending request: %v", requestErr)
+			}
+			if res.StatusCode != test.expectedStatus {
+				t.Errorf("Expected status %d, got %d", test.expectedStatus, res.StatusCode)
+			}
+
+			var users []models.UserInfo
+			decoderErr := json.NewDecoder(res.Body).Decode(&users)
+			if decoderErr != nil {
+				t.Fatalf("Error decoding response: %v", decoderErr)
+				return
+			}
+
+			if len(users) != len(test.expectedEmails) {
+				t.Fatalf("Expected %d users, got %d", len(test.expectedEmails), len(users))
+			}
+			for i, user := range users {
+				if user.Email != test.expectedEmails[i] {
+					t.Errorf("Expected email %s, got %s", test.expectedEmails[i], user.Email)
+				}
+			}
+		})
+	}
+}
+
+func BenchmarkDefaultUserHandler_GetUsers(b *testing.B) {
+	handler, err := DefaultUserHandler()
+	if err != nil {
+		b.Fatalf("Error creating user handler: %v", err)
+	}
+	app := fiber.New()
+	app.Post("/login", handler.Login())
+	app.Get("/users", Middleware(), handler.GetUsers())
+	tokens, err := testutils.LoginAsAdmin(app, "/login")
+	if err != nil {
+		b.Fatalf("Error logging in as admin: %v", err)
+	}
+
+	expectedEmails := []string{"user1@example.com", "jane_smith@example.com", "alex.wilson@example.com", "emma.davis@example.com"}
+
+	for b.Loop() {
+		params := url.Values{}
+		params.Add("limit", "4")
+		params.Add("page", "1")
+		res, requestErr := testutils.SendRequest(app, "/users?"+params.Encode(), "GET", tokens.AccessToken, nil)
+		if requestErr != nil {
+			b.Errorf("Error sending request: %v", requestErr)
+		}
+
+		var users []models.UserInfo
+		decoderErr := json.NewDecoder(res.Body).Decode(&users)
+		if decoderErr != nil {
+			b.Fatalf("Error decoding response: %v", decoderErr)
+			return
+		}
+
+		if len(users) != len(expectedEmails) {
+			b.Fatalf("Expected %d users, got %d", len(expectedEmails), len(users))
+		}
+
+		for i, user := range users {
+			if user.Email != expectedEmails[i] {
+				b.Errorf("Expected email %s, got %s", expectedEmails[i], user.Email)
+			}
+		}
+	}
+}
+
+func FuzzDefaultUserHandler_GetUsers(f *testing.F) {
+	handler, err := DefaultUserHandler()
+	if err != nil {
+		f.Fatalf("Error creating user handler: %v", err)
+	}
+	app := fiber.New()
+	app.Post("/login", handler.Login())
+	app.Get("/users", Middleware(), handler.GetUsers())
+	tokens, err := testutils.LoginAsAdmin(app, "/login")
+	if err != nil {
+		f.Fatalf("Error logging in as admin: %v", err)
+	}
+
+	f.Add(1, 1, "client")
+	f.Add(5, 5, "")
+	f.Add(10, 20, "role")
+	f.Add(50, 30, "1")
+	f.Fuzz(func(t *testing.T, limit, page int, role string) {
+		token := testutils.FilterToken(tokens.AccessToken)
+		params := url.Values{}
+		params.Add("limit", strconv.Itoa(limit))
+		params.Add("page", strconv.Itoa(page))
+		if role != "" {
+			role = "client"
+		} else if len(role) > 256 {
+			role = role[:256]
+		}
+		params.Add("role", role)
+
+		_, requestErr := testutils.SendRequest(app, "/users?"+params.Encode(), "GET", token, nil)
+		if requestErr != nil {
+			t.Errorf("Error sending request: %v", requestErr)
+		}
+	})
+}
---
+++ testutils/request.go
@@ -0,0 +1,57 @@
+package testutils
+
+import (
+	"api/models"
+	"bytes"
+	"encoding/json"
+	"github.com/gofiber/fiber/v2"
+	"io"
+	"net/http"
+	"net/http/httptest"
+)
+
+// SendRequest sends a request to a path with a specific method and JSON body.
+//
+// If the body is nil, the header Content-Type won't be set to application/json.
+func SendRequest(app *fiber.App, path, method, token string, body any) (*http.Response, error) {
+	var reader io.Reader
+
+	if body == nil {
+		reader = nil
+	} else {
+		data, err := json.Marshal(body)
+		if err != nil {
+			return nil, err
+		}
+		reader = bytes.NewReader(data)
+	}
+
+	var req *http.Request
+	if reader != nil {
+		req = httptest.NewRequest(method, path, reader)
+		req.Header.Set("Content-Type", "application/json")
+	} else {
+		req = httptest.NewRequest(method, path, nil)
+	}
+
+	if token != "" {
+		req.Header.Set("Authorization", "Bearer "+token)
+	}
+
+	return app.Test(req, -1)
+}
+
+// LoginAsAdmin send login request with a valid admin payload in testdata/users.json.
+func LoginAsAdmin(app *fiber.App, path string) (*models.TokenGroup, error) {
+	res, err := SendRequest(app, path, "POST", "", models.NewLoginUserPayload("john_doe", "Password1!"))
+	if err != nil {
+		return nil, err
+	}
+
+	var tokens models.TokenGroup
+	err = json.NewDecoder(res.Body).Decode(&tokens)
+	if err != nil {
+		return nil, err
+	}
+	return &tokens, nil
+}
