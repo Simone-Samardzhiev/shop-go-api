356452607be62266435039ed7bae7eed31111357
Simone <simonesamardzhiev@gmail.com>
2025-07-03 14:20:34 +0300

    Refactor: Remove the error message from the `InternalServerAPIError` function.

--- utils/api_error.go
+++ utils/api_error.go
@@ -27,8 +27,8 @@ func NewAPIErrorFromError(err error, status int) *APIError {
 }
 
 // InternalServerAPIError is the standard way of return a server error.
-func InternalServerAPIError(err error) *APIError {
-	return NewAPIErrorFromError(err, fiber.StatusInternalServerError)
+func InternalServerAPIError() *APIError {
+	return NewAPIError("Internal server error.", fiber.StatusInternalServerError)
 }
 
 // InvalidTokenAPIError is the standard way of return an invalid token error.
